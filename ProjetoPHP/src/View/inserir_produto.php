9d0ae40e0f472556c2c293fd8f72027febb86dfb
luis4PF <99516125+luis4PF@users.noreply.github.com>
2022-06-01 19:46:03 -0300

    commit

--- ProjetoPHP/src/View/inserir_produto.php
+++ ProjetoPHP/src/View/inserir_produto.php
@@ -8,35 +8,42 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Alterar Cliente</title>
+    <title>Inserir Produto</title>
   </head>
   <body>
       <?php require_once "barra_navegacao.php"; ?>
       <div class="container">
-        <h1>Alterar Cliente</h1>
+        <h1>Inserir Produto</h1>
 
-        <form action="/cliente/editar/<?= $params[1] ?>" method="post">
+        <form action="/produto/inserir" method="post">
             <div class="row">
+                <div class="col-5">
+                    <label for="id" class="label-control">
+                        Informe o ID:
+                    </label>
+                    <input type="number" class="form-control"
+                            name="id" id="id"/>
+                </div>
                 <div class="col-5">
                     <label for="nome" class="label-control">
                         Informe o nome:
                     </label>
                     <input type="text" class="form-control"
-                    name="nome" id="nome" value="<?= $resultado['nome'] ?>"/>
-                </div>
-                <div class="col-5">
-                    <label for="email" class="label-control">
-                        Informe o email:
-                    </label>
-                    <input type="text" class="form-control"
-                    name="email" id="email" value="<?= $resultado['email'] ?>"/>
+                            name="nome" id="nome"/>
                 </div>
                 <div class="col-2">
                     <label for="idade" class="label-control">
-                        Informe a idade:
+                        Informe o valor:
                     </label>
                     <input type="number" class="form-control"
-                            name="idade" id="idade" value="<?= $resultado['idade'] ?>"/>
+                            name="idade" id="idade"/>
+                </div>
+                <div class="col-5">
+                    <label for="descricao" class="label-control">
+                        Informe o nome:
+                    </label>
+                    <input type="text" class="form-control"
+                            name="descricao" id="descricao"/>
                 </div>
             </div>
             <div class="row">
